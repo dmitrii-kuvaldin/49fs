1e8ac4c62c6a81da77abc22af8bc22ae62a77f03
mohnapne <kuvaldin2012@gmail.com>
2024-11-04 10:29:00 +0100

    lesson 3

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";
+import Lesson01 from "./lessons/lesson01/Lesson01";
 import "./index.css";
-// импорт функции компонента
-import Lesson02 from "./lessons/lesson02/Lesson02";
+import Lesson03 from "./lessons/lesson03/Lesson03";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // этот пустой тег - это react фрагмент
-  // мы используем его для того чтобы по правилам react
-  // обернуть несколько дочерних компонентов в родитель
   <>
-    <App />
-    {/* вызов react компонента */}
-    {/* самозакрывающийся тег с именем функции-компонента */}
-    {/* не забудьте убедиться что у вас отработал импорт */}
-    <Lesson02 />
+    <Lesson01/>
+    {/* <Lesson02 /> */}
+    <Lesson03/>
   </>
 );
---
+++ src/lessons/lesson03/Lesson03.jsx
@@ -0,0 +1,10 @@
+function Lesson03() {
+  return (
+    <div className="lesson-container">
+      <h2>Lesson 03</h2>
+      <p>React props</p>
+    </div>
+  )
+}
+
+export default Lesson03;
