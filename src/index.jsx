d440b45bb0734ff31015cf594c95a873dde62a7c
mohnapne <kuvaldin2012@gmail.com>
2024-11-05 13:11:30 +0100

    lesson 3

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Lesson01 from "./lessons/lesson01/Lesson01";
 import "./index.css";
 // import Lesson03 from "./lessons/lesson03/Lesson03";
-import Homework03 from "./homeworks/homework03/Homework03";
+
+import Lesson04 from "./lessons/lesson04/Lesson04";
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <Lesson01/>
+    {/* <Lesson01/> */}
     {/* <Lesson03/> */}
-    <Homework03/>
+    <Lesson04/>
   </>
 );
